fe3093b58b0addd54574e7fed0a2cc623daea85e
monica chang <monica6423@gmail.com>
2022-12-15 14:29:28 +0800

    get company with parent company flag

--- src/handlers/get-companies.ts
+++ src/handlers/get-companies.ts
@@ -10,13 +10,22 @@ import { headers } from "../constants";
 export const getCompanies: APIGatewayProxyHandler = async (
   event: APIGatewayEvent
 ): Promise<APIGatewayProxyResult> => {
-  const args = parseQueryParams(event.queryStringParameters || {});
+  const parentCompanies = event.queryStringParameters?.parent;
+  if (parentCompanies === "true") {
+    const result = await prisma.company.findMany({
+      where: {
+        parentCompanyId: null,
+      },
+    });
 
-  const result = await prisma.company.findMany({
-    where: {
-      parentCompanyId: null,
-    },
-  });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result),
+      headers,
+    };
+  }
+
+  const result = await prisma.company.findMany();
 
   return {
     statusCode: 200,
